13235d4b17995d003477cfffbcdd2ab6223ca6a6
HADDOU <haddou.youness@outlook.fr>
2023-09-20 13:42:52 +0200

    save

---
+++ src/app/_shared/_components/large-loader/large-loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-large-loader',
+  templateUrl: './large-loader.component.html',
+  styleUrls: ['./large-loader.component.scss']
+})
+export class LargeLoaderComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LargeLoaderComponent } from './_shared/_components/large-loader/large-loader.component';
+import { ToastsComponent } from './_shared/_components/toasts/toasts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LargeLoaderComponent,
+    ToastsComponent,
   ],
   imports: [
     BrowserModule,
